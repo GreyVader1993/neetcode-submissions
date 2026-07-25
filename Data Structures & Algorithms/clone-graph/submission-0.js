619f82944675d0fb8478e7d8b82789bd13f6f5ad
Tanmay Dureja <45404936+GreyVader1993@users.noreply.github.com>
2026-07-24 17:39:24 -0700

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.js
@@ -0,0 +1,36 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val = 0, neighbors = []) {
+ *       this.val = val;
+ *       this.neighbors = neighbors;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node) {
+        let map = new Map()
+
+        function dfs(node){
+            if(map.has(node)){
+                return map.get(node)
+            }
+
+            let clone = new Node(node.val)
+            map.set(node, clone)
+
+            for(let neighbor of node.neighbors){
+                clone.neighbors.push(dfs(neighbor))
+            }
+
+            return clone
+        }
+
+        return node ? dfs(node) : null
+    }
+}
