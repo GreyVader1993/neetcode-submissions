9aae0aec0ff9505a96ee128871129c163bfc9c3d
Tanmay Dureja <45404936+GreyVader1993@users.noreply.github.com>
2026-08-12 14:52:09 -0700

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.js
@@ -0,0 +1,53 @@
+class TimeMap {
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key, value, timestamp) {
+        if(!this.keyStore.has(key)){
+            this.keyStore.set(key, [])
+        }
+        return this.keyStore.get(key).push({value, timestamp})
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key, timestamp) {
+        if(!this.keyStore.get(key)){
+            return ""
+        }
+
+        let list = this.keyStore.get(key)
+        let firstEl = list[0]
+
+        if(firstEl.timestamp > timestamp){
+            return ""
+        }
+
+        let left = 0
+        let right = list.length - 1
+        let res = ""
+
+        while(left <= right){
+            let mid = Math.floor((left + right) / 2)
+            if(list[mid].timestamp === timestamp){
+                return list[mid].value
+            } else if (list[mid].timestamp <= timestamp){
+                left = mid + 1
+                res = list[mid].value
+            } else {
+                right = mid - 1
+            }
+        }
+        return res
+    }
+}
